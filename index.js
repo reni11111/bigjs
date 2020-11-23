9d4c40371f82f2fa6567b4fa87cfa92662f30b9d
reni11111 <renaldo@tegeria.com>
2020-11-23 12:38:46 +0100

    algorithm with precision

--- index.js
+++ index.js
@@ -1,123 +1,282 @@
 const Big = require('big.js')
 
 let order = {
-  id: "orderTestId",
   "lineItems": [
     {
+      "itemName": "Laps",
+      "quantity": 1,
+      "barcode": "12345678",
+      "itemId": "1kU0VcdLD1V0EnFFtEpcKsa0Gez",
+      "variationName": "Regular",
+      "variationId": "1kU0Vc8NXbeGkRtxxlWD9Rt9MIN",
+      "basePriceMoney": {
+        "amount": 20,
+        "currency": "Lekë"
+      },
+      "modifiers": [],
+      "appliedTax": {
+        "id": "Tax20%",
+        "name": "20%",
+        "percentage": 20
+      },
+      "appliedDiscounts": [],
+      "totalTaxMoney": {
+        "amount": 4,
+        "currency": "Lekë"
+      },
+      "totalDiscountMoney": {
+        "amount": 0,
+        "currency": "Lekë"
+      },
+      "totalMoney": {
+        "amount": 24,
+        "currency": "Lekë"
+      },
+      "imageUrl": null,
+      "labelColor": "#EF5350",
+      "taxInclusionType": "ADDITIVE"
+    },
+    {
+      "itemName": "Bllok",
+      "quantity": 1,
+      "barcode": null,
+      "itemId": "1kYuMp3h3lPKqv9cXB76nYbTe2n",
+      "variationName": "Txtixigxkggkchchi",
+      "variationId": "1kZF6ixFHhdn5n7RheW45yrt1jG",
+      "basePriceMoney": {
+        "amount": 88,
+        "currency": "Lekë"
+      },
+      "modifiers": [],
+      "appliedTax": {
+        "id": "Tax0%",
+        "name": "0%",
+        "percentage": 0
+      },
+      "appliedDiscounts": [],
+      "totalTaxMoney": {
+        "amount": 0,
+        "currency": "Lekë"
+      },
+      "totalDiscountMoney": {
+        "amount": 0,
+        "currency": "Lekë"
+      },
+      "totalMoney": {
+        "amount": 88,
+        "currency": "Lekë"
+      },
+      "imageUrl": "https://item-bucket-saas.s3.eu-central-1.amazonaws.com/1g2VqQJbzs3rKtQOklfNUhbzD8I_1kYuMp3h3lPKqv9cXB76nYbTe2n_1kZGOCHl6PpJy93u95rXRFSnJAQ.jpg",
+      "labelColor": null,
+      "taxInclusionType": "INCLUSIVE"
+    },
+    {
+      "itemName": "Hamburger",
+      "quantity": 1,
+      "barcode": "987654321",
+      "itemId": "1kUAAkxz5tkPoAPks3A92uY0Qcf",
+      "variationName": "Sandwich 11",
+      "variationId": "1kZBslrv9GrDqLbNGiH64QExdLi",
+      "basePriceMoney": {
+        "amount": 600,
+        "currency": "Lekë"
+      },
+      "modifiers": [
+        {
+          "id": "1kU8z2VxYbrJjDHJN75zUmmei07",
+          "name": "Speca",
+          "modifierListId": "1kU8yxUoYmgdAkbIYHg2Q0eIIXb",
+          "modifierListName": "Perime ekstra",
+          "appliedMoney": {
+            "amount": 30,
+            "currency": "Lekë"
+          }
+        },
+        {
+          "id": "1kU8z49LSVB7xH46fZBOHLaMYfr",
+          "name": "Sallate",
+          "modifierListId": "1kU8yxUoYmgdAkbIYHg2Q0eIIXb",
+          "modifierListName": "Perime ekstra",
+          "appliedMoney": {
+            "amount": 10,
+            "currency": "Lekë"
+          }
+        },
+        {
+          "id": "1kU8z6Pigs3BF2OGZ1FlpoueeFH",
+          "name": "Domate",
+          "modifierListId": "1kU8yxUoYmgdAkbIYHg2Q0eIIXb",
+          "modifierListName": "Perime ekstra",
+          "appliedMoney": {
+            "amount": 20,
+            "currency": "Lekë"
+          }
+        }
+      ],
+      "appliedTax": {
+        "id": "Tax20%",
+        "name": "20%",
+        "percentage": 20
+      },
+      "appliedDiscounts": [
+        {
+          "id": "1kU29V4aEYpfkOjbWZdJFEGU3FM",
+          "name": "Black friday",
+          "discountType": "FIXED_PERCENTAGE",
+          "percentage": 5,
+          "appliedMoney": null
+        }
+      ],
+      "totalTaxMoney": {
+        "amount": 125.4,
+        "currency": "Lekë"
+      },
+      "totalDiscountMoney": {
+        "amount": 33,
+        "currency": "Lekë"
+      },
+      "totalMoney": {
+        "amount": 752.4,
+        "currency": "Lekë"
+      },
+      "imageUrl": null,
+      "labelColor": null,
+      "taxInclusionType": "ADDITIVE"
+    },
+    {
+      "itemName": "Stilolaps",
+      "quantity": 1,
+      "barcode": null,
+      "itemId": "1kcLe2SGb9w4k9UpkR3s88s4qw0",
+      "variationName": "Regular",
+      "variationId": "1kcLe47e2jqyuOXu3mIR8uvmF2Z",
+      "basePriceMoney": {
+        "amount": 200,
+        "currency": "Lekë"
+      },
+      "modifiers": [],
+      "appliedTax": {
+        "id": "Tax10%",
+        "name": "10%",
+        "percentage": 10
+      },
+      "appliedDiscounts": [],
+      "totalTaxMoney": {
+        "amount": 18.18,
+        "currency": "Lekë"
+      },
+      "totalDiscountMoney": {
+        "amount": 0,
+        "currency": "Lekë"
+      },
+      "totalMoney": {
+        "amount": 200,
+        "currency": "Lekë"
+      },
+      "imageUrl": null,
+      "labelColor": "#6CCDFE",
+      "taxInclusionType": "INCLUSIVE"
+    },
+    {
       "itemName": "Fletore vizatimi",
-      "quantity": 2,
+      "quantity": 1,
       "barcode": "12345679",
       "itemId": "1kU2Bx8kDVAQkfN5t2DgHeG0FQj",
       "variationName": "Regular",
       "variationId": "1kU2BwD6bQRH3jb6eimQpw8AIXG",
       "basePriceMoney": {
-          "amount": 100,
-          "currency": "Lekë"
+        "amount": 350,
+        "currency": "Lekë"
       },
       "modifiers": [],
       "appliedTax": {
-          "id": "Tax0%",
-          "name": "0%",
-          "percentage": 10
+        "id": "Tax20%",
+        "name": "20%",
+        "percentage": 20
       },
       "appliedDiscounts": [
-          // {
-          //     "id": "1kU99q9mA7FYLKElokwQ6NM3ZDy",
-          //     "name": "100 leke ulje",
-          //     "discountType": "FIXED_AMOUNT",
-          //     "percentage": null,
-          //     "appliedMoney": {
-          //         "amount": 100,
-          //         "currency": "Lekë"
-          //     }
-          // }
+        {
+          "id": "1kU99q9mA7FYLKElokwQ6NM3ZDy",
+          "name": "350 leke ulje",
+          "discountType": "FIXED_AMOUNT",
+          "percentage": null,
+          "appliedMoney": {
+            "amount": 350,
+            "currency": "Lekë"
+          }
+        }
       ],
       "totalTaxMoney": {
-          "amount": 18.18,
-          "currency": "Lekë"
+        "amount": 0,
+        "currency": "Lekë"
       },
       "totalDiscountMoney": {
-          "amount": 0,
-          "currency": "Lekë"
+        "amount": 350,
+        "currency": "Lekë"
       },
       "totalMoney": {
-          "amount": 200,
-          "currency": "Lekë"
+        "amount": 0,
+        "currency": "Lekë"
       },
       "imageUrl": null,
       "labelColor": "#6CCDFE",
-      "taxInclusionType": "INCLUSIVE",
-      // "priceWithItemAmountDiscount": 900
-    },
-    {
-      "itemName": "Molle",
-      "quantity": 3,
-      "itemId": "itemi 2",
-      "variationName": "Molle e kuqe",
-      "variationId": "1hBQW8hgPDEOvLQUYmGANSDdEnW",
-      "basePriceMoney": {
-        "amount": 20,
-        "currency": "Leke"
-      },
-      "taxInclusionType": "ADDITIVE",
-      "modifiers": [],
-      "appliedTax": {
-        "id": "1hBOAwi9bdZsWVTHBjgHGfNGyyC",
-        "name": "TVSH",
-        "percentage": 20
-      },
-      "appliedDiscounts": [],
-      "totalTaxMoney": {
-        "amount": 12,
-        "currency": "Leke"
-      },
-      "totalDiscountMoney": {
-        "amount": 0,
-        "currency": "Leke"
-      },
-      "totalMoney": {
-        "amount": 72,
-        "currency": "Leke"
-      }
-    },
+      "taxInclusionType": "ADDITIVE"
+    }
   ],
   "locationId": "J11111111A",
-  "appliedDiscounts": [
-    // {
-    //   "id": "1hH8DsoxX9gzXdVnTzZGyYfjS7e",
-    //   "name": "100 Leke ulje",
-    //   "discountType": "FIXED_AMOUNT",
-    //   "percentage": null,
-    //   "appliedMoney": {
-    //     "amount": 100,
-    //     "currency": "Leke"
-    //   }
-    // },
-    // {
-    //   "id": "1gEUQ8qBRHC6ROYws9MRQ3zGXeC",
-    //   "name": "Drinks Discount",
-    //   "discountType": "FIXED_PERCENTAGE",
-    //   "percentage": 50,
-    //   "appliedMoney": {
-    //     "amount": 2,
-    //     "currency": "EURO"
-    //   }
-    // }
-  ],
+  "appliedDiscounts": [],
   "totalMoney": {
-    "amount": 272,
-    "currency": "Leke"
+    "amount": 1064.4,
+    "currency": "Lekë"
   },
   "totalTaxMoney": {
-    "amount": 30.18,
-    "currency": "Leke"
+    "amount": 147.58,
+    "currency": "Lekë"
   },
   "totalDiscountMoney": {
-    "amount": 0,
-    "currency": "Leke"
-  }
-
+    "amount": 383,
+    "currency": "Lekë"
+  },
+  "orderGroupedTaxes": [
+    {
+      "VATRate": 0,
+      "NumOfItems": 1,
+      "PriceBefVat": {
+        "amount": 88,
+        "currency": "Lekë"
+      },
+      "VATAmt": {
+        "amount": 0,
+        "currency": "Lekë"
+      }
+    },
+    {
+      "VATRate": 10,
+      "NumOfItems": 1,
+      "PriceBefVat": {
+        "amount": 180,
+        "currency": "Lekë"
+      },
+      "VATAmt": {
+        "amount": 20,
+        "currency": "Lekë"
+      }
+    },
+    {
+      "VATRate": 20,
+      "NumOfItems": 3,
+      "PriceBefVat": {
+        "amount": 518,
+        "currency": "Lekë"
+      },
+      "VATAmt": {
+        "amount": 129,
+        "currency": "Lekë"
+      }
+    }
+  ]
 }
+
 
 let orderTotalMoney = 0
 let orderTotalTax = 0
@@ -132,21 +291,21 @@ let lineItemsWithItemAmountDiscount = order.lineItems.map(orderLineItem => {
   orderLineItem.modifiers = orderLineItem.modifiers || []
   orderLineItem.appliedDiscounts = orderLineItem.appliedDiscounts || []
 
+  // percentage might be 0.1% or less so DP=2 would round that to 0
+  Big.DP = 10
+
   const modifiersPrice = orderLineItem.modifiers.reduce((sum, { appliedMoney }) => +Big(sum).plus(appliedMoney.amount), 0)
 
   const basePrice = +Big(orderLineItem.basePriceMoney.amount).plus(modifiersPrice)
 
-  const priceWithQuantity = +Big(basePrice).times(orderLineItem.quantity).round(0)
-
-  // percentage might be 0.1% or less so DP=2 would round that to 0
-  Big.DP = 10
+  const priceWithQuantity = +Big(basePrice).times(orderLineItem.quantity)
 
   // when multiple appliedDiscounts apply the next discount the the product who has been discounted
   // by the previous discount
   const itemPercentageDiscounts = orderLineItem.appliedDiscounts.filter(discount => discount.discountType === 'FIXED_PERCENTAGE' || discount.discountType === 'VARIABLE_PERCENTAGE' )
   const priceWithItemDiscounts = +Big(itemPercentageDiscounts.reduce((itemPrice, { percentage }) => {
     const discountPercentage = +Big(percentage).div(100)
-    const discountValue = Big(itemPrice).times(discountPercentage).round(0)
+    const discountValue = Big(itemPrice).times(discountPercentage)
     return+Big(itemPrice).minus(discountValue)
   }, priceWithQuantity))
 
@@ -155,7 +314,7 @@ let lineItemsWithItemAmountDiscount = order.lineItems.map(orderLineItem => {
   const orderPercentageDiscounts = order.appliedDiscounts.filter(discount => discount.discountType === 'FIXED_PERCENTAGE' || discount.discountType === 'VARIABLE_PERCENTAGE')
   const priceWithOrderPercentageDiscounts = +Big(orderPercentageDiscounts.reduce((itemPrice, { percentage }) => {
     const discountPercentage = +Big(percentage).div(100)
-    const discountValue = Big(itemPrice).times(discountPercentage).round(0)
+    const discountValue = Big(itemPrice).times(discountPercentage)
     return+Big(itemPrice).minus(discountValue)
   }, priceWithItemDiscounts))
 
@@ -188,7 +347,7 @@ const orderAmountDiscounts = order.appliedDiscounts.filter(discount => discount.
 const orderDiscountAmountsTotal = +Big(orderAmountDiscounts.reduce((price, { appliedMoney }) => {
   return+Big(price).plus(appliedMoney.amount)
 }, 0))
-orderTotalDiscount = +Big(orderTotalDiscount).plus(orderDiscountAmountsTotal).round(0)
+orderTotalDiscount = +Big(orderTotalDiscount).plus(orderDiscountAmountsTotal)
 
 let groupedTaxes ={}
 
@@ -198,13 +357,14 @@ lineItemsWithItemAmountDiscount.map(orderLineItem=>{
     let itemCurrentSubtotalPortion = +new Big(orderLineItem.priceWithItemAmountDiscount).div(orderCurrentSubtotal)
     let itemOrderDiscount = + new Big(itemCurrentSubtotalPortion).times(appliedMoney.amount)
 
-    return+Big(itemPrice).minus(itemOrderDiscount).round(0)
+    return+Big(itemPrice).minus(itemOrderDiscount)
   }, orderLineItem.priceWithItemAmountDiscount))
 
   let priceWithTax = priceWithOrderAmountDiscounts
   if (orderLineItem.appliedTax) {
     let basePriceValue, taxValue
-
+    
+    // grouped taxes
     if (orderLineItem.taxInclusionType === 'ADDITIVE') {
       const taxPercentage = +Big(orderLineItem.appliedTax.percentage).div(100)
       // UPDATE ORDER TOTAL TAX
@@ -213,22 +373,22 @@ lineItemsWithItemAmountDiscount.map(orderLineItem=>{
       // values will be used at grouped taxes
       basePriceValue = priceWithOrderAmountDiscounts
 
-      priceWithTax = +Big(priceWithTax).plus(taxValue).round(0)
+      priceWithTax = +Big(priceWithTax).plus(taxValue)
     } else if(orderLineItem.taxInclusionType === 'INCLUSIVE'){
       console.log("ERDHII")
       // inclusive basePrice price/(1+vat/100)
-      basePriceValue = +Big(priceWithOrderAmountDiscounts).div(+Big(1).plus(+Big(orderLineItem.appliedTax.percentage).div(100))).round(2)
+      basePriceValue = +Big(priceWithOrderAmountDiscounts).div(+Big(1).plus(+Big(orderLineItem.appliedTax.percentage).div(100)))
       // inclusive vatValue price - basePrice
-      taxValue = +Big(priceWithOrderAmountDiscounts).minus(basePriceValue).round(2)
+      taxValue = +Big(priceWithOrderAmountDiscounts).minus(basePriceValue)
     }
 
     orderTotalTax = +Big(orderTotalTax).plus(taxValue)
 
-    if (!Big(taxValue).eq(orderLineItem.totalTaxMoney.amount)) {
-      throw new Error(`item inclusive applied TAX ${orderLineItem.itemId} calculated wrong!, should be ${taxValue}`)
+    const roundedItemTax = +Big(taxValue).round(2)
+    if (!Big(roundedItemTax).eq(orderLineItem.totalTaxMoney.amount)) {
+      throw new Error(`item inclusive applied TAX ${orderLineItem.itemId} calculated wrong!, should be ${roundedItemTax}`)
     }
 
-        // grouped taxes
     if (!(orderLineItem.appliedTax.percentage in groupedTaxes)) {
       console.log("not included")
       groupedTaxes[orderLineItem.appliedTax.percentage] = {
@@ -249,6 +409,10 @@ lineItemsWithItemAmountDiscount.map(orderLineItem=>{
 
   orderTotalMoney = +Big(orderTotalMoney).plus(priceWithTax)
 
+  // rounding (add totalMoney to order object ) 
+  priceWithTax = +Big(priceWithTax).round(2)
+  // HEY SHEFI IN HERE ADD the totalTaxMoney and totalDiscountMoney ROUNDED TO THE ITEM
+
   console.log('final pricing with tax', priceWithTax)
   if(Big(priceWithTax).lt(0)){
     throw new Error(`item ${JSON.stringify(orderLineItem)} price should not be under 0!`)
@@ -259,6 +423,11 @@ lineItemsWithItemAmountDiscount.map(orderLineItem=>{
   }
 
 })
+
+// rounding (add this to  order object)
+orderTotalMoney = +Big(orderTotalMoney).round(2)
+orderTotalDiscount = +Big(orderTotalDiscount).round(2)
+orderTotalTax = +Big(orderTotalTax).round(2)
 
 console.log('order total money', orderTotalMoney)
 console.log('order total discount', orderTotalDiscount)
@@ -277,6 +446,14 @@ if(!Big(order.totalDiscountMoney.amount).eq(orderTotalDiscount)) {
 }
 
 // convert to array
-groupedTaxes = Object.keys(groupedTaxes).map(key=> groupedTaxes[key])
+groupedTaxes = Object.keys(groupedTaxes).map(key=> {
+  let tax = groupedTaxes[key]
+  // rounding taxes (add this to order object)
+  return {
+    ...tax,
+    PriceBefVat: +Big(tax.PriceBefVat).round(2),
+    VATAmt: +Big(tax.VATAmt).round(2)
+  }
+})
 
 console.log(groupedTaxes)
